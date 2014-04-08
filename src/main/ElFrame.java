1a2f26fbf79e88577b4070c3c761d96d98099f64
Fredrik Kloster <fredrik_kloster@hotmail.com>
2014-04-08 13:54:17 +0200

    added frame

---
+++ src/main/ElFrame.java
@@ -0,0 +1,59 @@
+package main;
+
+import java.awt.Dimension;
+import java.awt.GraphicsConfiguration;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import java.util.Collection;
+
+import javax.swing.JFrame;
+
+public class ElFrame extends JFrame implements Runnable {
+
+	private boolean running;
+	
+	public static final int BLOCKSIZE = 32;
+	private Map map;
+	private Dimension leMapSize;
+	
+	public ElFrame(){
+		setTitle("Blades of Darkness");
+		setResizable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		leMapSize  = new Dimension(BLOCKSIZE*10,BLOCKSIZE*10);	
+		initialize();
+		
+		
+	}
+	
+	public void initialize(){
+		setLayout((new GridLayout(1,1)));
+		//FILL
+		map = new Map();
+		add(map);
+		//FILL
+		
+		setSize(leMapSize);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+	@Override
+	public void run() {
+		
+
+	}
+	
+	public synchronized void start(){
+		running = true;
+		new Thread(this).start();
+	}
+	public synchronized void stop(){
+		running = false;
+	}
+	public static void main(String[] args) {
+		new ElFrame().start();
+
+	}
+
+}
---
+++ src/main/Map.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.awt.LayoutManager;
+import java.awt.*;
+
+import javax.swing.JPanel;
+
+
+public class Map extends JPanel {
+	private int mapSizeX, mapSizeY, blockSize;
+	private int mapArray[][];
+	
+	public Map() {
+		mapSizeX = 10;
+		mapSizeY = 10;
+		blockSize = ElFrame.BLOCKSIZE;
+		mapArray = new int [mapSizeY][mapSizeX];
+		defineMap();
+	}
+	
+	public void draw(){
+		repaint();
+	}
+	
+	public void defineMap(){
+		
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		for (int row = 0; row < (mapSizeY*blockSize); row+=blockSize){
+			for (int col = 0; col < (mapSizeX*blockSize);col+=blockSize){
+				g.drawRect(row, col, blockSize, blockSize);
+				
+			}
+			
+		}
+		
+		
+	}
+
+}
